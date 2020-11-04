1091f909e0fa9921c9b611fa42cbe5deb712a452
Vadzim Mas <mas.usa.gov@gmail.com>
2020-11-04 09:21:56 -0500

    Initial commit

---
+++ #source/js/main.js
@@ -0,0 +1,16 @@
+
+// @prepros -prepend lazysizes.min.js
+// @prepros -prepend jquery-3.5.1.min.js
+// @prepros-prepend parallax.js
+// @prepros -append jquery.parallax.js
+
+
+
+// @prepros -prepend slick.js
+// @prepros -prepend slick-settings.js
+
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+
