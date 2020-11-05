74bdb5a06759ea8d94d516e6288837eb5e915c67
Vadzim Mas <mas.usa.gov@gmail.com>
2020-11-05 14:01:03 -0500

    1

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,6 +1,6 @@
 
 // @prepros -prepend lazysizes.min.js
-// @prepros -prepend jquery-3.5.1.min.js
+// @prepros-prepend jquery-3.5.1.min.js
 // @prepros-prepend parallax.js
 // @prepros -append jquery.parallax.js
 
@@ -15,7 +15,23 @@
 // console.log(container);
 
 
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
 
+
+
+
+2
+3
+$(document).ready(function () {
+    $(".wrapper").addClass("active");
+    $(".parralax__liighthouse .img").addClass("active");
+    $(".wave .img").addClass("active");
+    $(".rope").addClass("active");
+    $(".light-1").addClass("active");
+    $(".light-2").addClass("active");
+    $(".light-3").addClass("active");
+
+
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+});
 
