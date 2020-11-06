f33a6b33fb39af94111f47f4e1484489d17298bd
Vadzim Mas <mas.usa.gov@gmail.com>
2020-11-06 13:00:08 -0500

    1

--- #source/js/main.js
+++ #source/js/main.js
@@ -20,6 +20,8 @@ $(document).ready(function () {
     $(".wrapper").addClass("active");
     $(".parralax__liighthouse .img-l").addClass("active");
     $(".wave .img").addClass("active");
+    $(".rope1-wrapper").addClass("active");
+    $(".rope2-wrapper").addClass("active");
     $(".rope3-wrapper").addClass("active");
     $(".light-1").addClass("active");
     $(".light-2").addClass("active");
@@ -28,6 +30,8 @@ $(document).ready(function () {
     setTimeout(() => {
         let lighthouse = document.getElementsByClassName("parralax__liighthouse")[0].getElementsByClassName("img-l")[0];
         let wave = document.getElementsByClassName("wave");
+        let rope1wrapper = document.getElementsByClassName("rope1-wrapper")[0];
+        let rope2wrapper = document.getElementsByClassName("rope2-wrapper")[0];
         let rope3wrapper = document.getElementsByClassName("rope3-wrapper")[0];
         let light1 = document.getElementsByClassName("light-1")[0];
         let light2 = document.getElementsByClassName("light-2")[0];
@@ -39,14 +43,16 @@ $(document).ready(function () {
             let w = wave[i].getElementsByClassName("img-w")[0];
             w.style.transition = "none";
         }
+        rope1wrapper.style.transition = 'none';
+        rope2wrapper.style.transition = 'none';
         rope3wrapper.style.transition = 'none';
         light1.style.transition = "none";
         light2.style.transition = "none";
         light3.style.transition = "none";
-    }, 4500);
+    }, 10500);
 
 
-    // var scene = document.getElementById('scene');
-    // var parallaxInstance = new Parallax(scene);
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
 });
 
