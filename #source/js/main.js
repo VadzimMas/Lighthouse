59e2e96771a6b6bb2a11b7173d461781352ee45c
Vadzim Mas <mas.usa.gov@gmail.com>
2020-11-07 11:46:57 -0500

    1

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,6 +1,6 @@
 
 // @prepros -prepend lazysizes.min.js
-// @prepros -prepend jquery-3.5.1.min.js
+// @prepros-prepend jquery-3.5.1.min.js
 // @prepros-prepend parallax.js
 // @prepros -prepend slick.js
 // @prepros -prepend slick-settings.js
@@ -10,12 +10,8 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
-window.onorientationchange = function () {
-    var orientation = window.orientation;
-    switch (orientation) {
-        case 0:
-        case 90: window.location.reload();
-        case -90: window.location.reload();
-            break;
-    }
-};
+
+
+$(window).resize(function () {
+    window.location.reload();
+});
